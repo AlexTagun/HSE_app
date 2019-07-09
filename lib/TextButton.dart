90c2919117201c81d2a4e8d1acc9446ac5a04915
Alex Tagun <alextagun@gitlab.com>
2019-07-09 20:42:11 +0300

    main menu frontend

---
+++ lib/TextButton.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class TextButton extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new TextButtonState();
+  }
+}
+
+class TextButtonState extends State<TextButton> {
+  int number = 0;
+
+  void subtractNumbers() {
+    setState(() {
+      number = number - 1;
+    });
+  }
+
+  void addNumbers() {
+    setState(() {
+      number = number + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("Raised Button"),
+        ),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new RaisedButton(
+                    padding: const EdgeInsets.all(8.0),
+                    textColor: Colors.white,
+                    color: Colors.blue,
+                    onPressed: addNumbers,
+                    child: new Text("Викторина"),
+                  ),
+
+                ],
+              ),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new RaisedButton(
+                    padding: const EdgeInsets.all(8.0),
+                    textColor: Colors.white,
+                    color: Colors.blue,
+                    onPressed: addNumbers,
+                    child: new Text("Второй режим"),
+                  ),
+
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+}
