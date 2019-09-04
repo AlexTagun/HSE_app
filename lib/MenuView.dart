f100aa59bd87af369a3fffb567c5958bc4e3e55d
alextagun <alextagun@gmail.com>
2019-09-04 17:40:07 +0300

    add button "About us" add lab logo on the menu screen

--- lib/MenuView.dart
+++ lib/MenuView.dart
@@ -4,6 +4,7 @@ import 'TruthOrLieQuizView.dart';
 
 import 'package:hse_app/LoadManager.dart';
 import 'package:hse_app/QuizManager.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MainView extends StatefulWidget {
   @override
@@ -41,6 +42,15 @@ class MainViewState extends State<MainView> {
     }
   }
 
+  _launchURL() async {
+    const url = 'https://lap.hse.ru/about';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -50,46 +60,60 @@ class MainViewState extends State<MainView> {
         ),
         body: new Center(
           child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+//            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
 
               new Image.asset(
                 'assets/res/hse_logo.png',
                 fit: BoxFit.contain,
               ),
-
-              new Text("Проектно-учебная лаборатория антикоррупционной политики",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                textAlign: TextAlign.center,
+              new Image.asset(
+                'assets/res/lab_logo.png',
+//                fit: BoxFit.contain,
               ),
+
+
+//              new Text("Проектно-учебная лаборатория антикоррупционной политики",
+//                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+//                textAlign: TextAlign.center,
+//              ),
 
               new Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  new Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: <Widget>[
-                        new RaisedButton(
-                          padding: const EdgeInsets.all(8.0),
-                          textColor: Colors.white,
-                          color: Colors.blue,
-                          onPressed: toCountryQuizView,
-                          child: new Text("Викторина"),
-                        ),
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                          new RaisedButton(
+                            padding: const EdgeInsets.all(8.0),
+                            textColor: Colors.white,
+                            color: Colors.blue,
+                            onPressed: toCountryQuizView,
+                            child: new Text("Угадай страну"),
+                          ),
 
-                        new RaisedButton(
-                          padding: const EdgeInsets.all(8.0),
-                          textColor: Colors.white,
-                          color: Colors.blue,
-                          onPressed: toTrueOrLieQuizView,
-                          child: new Text("Второй режим"),
-                        ),
-                      ],
-                  ),
-                ],
-              ),
+                          new RaisedButton(
+                            padding: const EdgeInsets.all(8.0),
+                            textColor: Colors.white,
+                            color: Colors.blue,
+                            onPressed: toTrueOrLieQuizView,
+                            child: new Text("Верю - не верю"),
+                          ),
+                          new RaisedButton(
+                            padding: const EdgeInsets.all(8.0),
+                            textColor: Colors.white,
+                            color: Colors.blue,
+                            onPressed: _launchURL,
+                            child: new Text("О нас"),
+                          ),
+                        ],
+                    ),
+                  ],
+                ),
             ],
           ),
-        ));
+        )
+    );
   }
 }
